d2b3b98b61118810975bc6ed5d9bc074722da59d
Chris Rice <chrisrice93@gmail.com>
2015-12-03 17:51:49 -0700

    lets call that good.

--- web/index.php
+++ web/index.php
@@ -1,34 +1,82 @@
 <!DOCTYPE html>
 <html>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
-    <!-- Latest compiled JavaScript -->
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-    <head>
-        <style type="text/css">
-            html, body { height: 90%; margin: 0; padding: 0; }
-            #map { height: 100%; }
-            #lat-lon-input {
-                width: 300px;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="map"></div>
-        <div id="interface">
-            <div class="form-group" id="lat-lon-input">
-                <?php include 'populate_crimes.php';?>
-                <button type="button" class="btn btn-default" id="show_btn">Show Crimes</button>
-            </div>
+<head>
+    <style type="text/css">
+        html,
+        body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
 
+        #map {
+            position: absolute;
+            right: 0px;
+            height: 100%;
+            width: 80%;
+        }
+
+        #interface {
+            position: absolute;
+            left: 0px;
+            width: 20%;
+        }
+
+        #theform {
+            padding: 10px;
+        }
+
+        #show_btn {
+            margin-top: 10px;
+            position: absolute;
+            right: 0px;
+        }
+
+        #button_and_checkboxes {
+            position: relative;
+            display: block;
+        }
+
+        #splitter_thing {
+            height: 2px;
+            background-color: rgb(128, 128, 128);
+        }
+    </style>
+</head>
+
+<body>
+    <div id="map"></div>
+    <div id="interface">
+        <div class="form-group" id="theform">
+            <?php include 'populate_crimes.php';?>
+                <div id="button_and_checkboxes">
+                    <button type="button" class="btn btn-default" id="show_btn">Show</button>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value="crimes" id="crimes_check" checked>Crimes</label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value="traffic_accidents" id="traffic_check" checked>Traffic Accidents</label>
+                    </div>
+                </div>
+                <div id="splitter_thing"></div>
+                <h1 id="crime_counter">0 records</h1>
         </div>
-        <script type="text/javascript" src="maps_loader.js"/></script>
-        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRIAh31ZR7A5cyxNBJCePfSvP1ZoSM9z8&callback=initMap">
-        </script>
-    </body>
+
+    </div>
+    <script type="text/javascript" src="maps_loader.js" /></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRIAh31ZR7A5cyxNBJCePfSvP1ZoSM9z8&callback=initMap">
+    </script>
+</body>
+
 </html>
--- web/maps_loader.js
+++ web/maps_loader.js
@@ -1,4 +1,5 @@
 var map;
+var markers = new Array();
 
 function getLatitude() {
     return parseFloat($('#lat').val());
@@ -9,11 +10,14 @@ function getLongitude() {
 }
 
 function getMarkerCoordinates() {
-    return {lat: getLatitude(), lng: getLongitude()}
+    return {
+        lat: getLatitude(),
+        lng: getLongitude()
+    }
 }
 
 function getDenverCoordinates() {
-    return {lat: 39.723, lng: -105.04}
+    return packageCoordinates(39.7500956, -105.0021028);
 }
 
 function initMap() {
@@ -23,7 +27,17 @@ function initMap() {
     });
 }
 
+function packageCoordinates(latitude, longitude) {
+    return {
+        lat: latitude,
+        lng: longitude
+    };
+
+}
+
 function putMarkersForCrime() {
+    setMapOnAll(null);
+
     full_code = $("#crime_select").val()
 
     // Split the code and ext code
@@ -31,9 +45,14 @@ function putMarkersForCrime() {
     offense_code = code_array[0];
     offense_ext = code_array[1];
 
-    $.get("get_locations.php", { code:offense_code, ext:offense_ext },
-        function(data){
-            addCrimeMarkers(data)
+    $.get("get_locations.php", {
+            code: offense_code,
+            ext: offense_ext,
+            crimes: $("#crimes_check").is(':checked'),
+            traffic: $("#traffic_check").is(':checked')
+        },
+        function(data) {
+            addCrimeMarkers(data);
         }
     );
 }
@@ -41,9 +60,35 @@ function putMarkersForCrime() {
 function addCrimeMarkers(data) {
     // data = "{\"locations\": [{\"lat\":102, \"lon\":103}]}"
     // console.log(data);
-    markers = JSON.parse(data);
-    console.log(markers);
-    
+    markers_from_php = JSON.parse(data);
+    console.log("Received " + markers_from_php.locations.length + " results");
+
+    num_records = markers_from_php.locations.length;
+    if (num_records == 1) {
+        $("#crime_counter").text(markers_from_php.locations.length + " record");
+    } else {
+        $("#crime_counter").text(markers_from_php.locations.length + " records");
+    }
+
+    for (i = 0; i < markers_from_php.locations.length; i++) {
+        // console.log(markers_from_php.locations[i]);
+        latitude = markers_from_php.locations[i].latitude;
+        longitude = markers_from_php.locations[i].longitude;
+        addMarker(packageCoordinates(latitude, longitude));
+    }
+
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+function clearMarkers() {
+    setMapOnAll(null);
+    markers = [];
 }
 
 function addMarker(coordinates) {
@@ -53,8 +98,17 @@ function addMarker(coordinates) {
         position: coordinates,
         title: 'CRIMEEEEEE!'
     });
+    markers.push(marker);
 }
 
 $("#show_btn").click(function() {
+    putMarkersForCrime();
+});
+
+$("#crimes_check").click(function() {
+    putMarkersForCrime();
+});
+
+$("#traffic_check").click(function() {
     putMarkersForCrime();
 })
